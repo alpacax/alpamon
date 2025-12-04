698f585e7160aa561656348719530bce0200f217
Apdul0329 <shrmsdn2067@gmail.com>
2025-12-04 13:40:49 +0900

    feat(handler): add InfoHandler and SystemInfoAdapter
    Add a InfoHandler to process system
    information-related commands.
    This handler takes over the following cases previously
    handled by CommandRunner: ping, help, commit, sync.
    Unlike other handlers, the InfoHandler does not directly
    execute commands on the system via the Executor.
    Instead, it handles system information through the
    SystemInfoManager.
    SystemInfoAdapter is an implementation of the
    SystemInfoManager interface.

---
+++ pkg/executor/handlers/info/adapter.go
@@ -0,0 +1,39 @@
+package info
+
+import (
+	"github.com/alpacax/alpamon/pkg/scheduler"
+)
+
+// SystemInfoAdapter implements the SystemInfoManager interface for handlers
+type SystemInfoAdapter struct {
+	session            *scheduler.Session
+	commitFunc         func()
+	syncFunc           func(*scheduler.Session, []string)
+}
+
+// NewSystemInfoAdapter creates a new system info adapter with function callbacks
+func NewSystemInfoAdapter(
+	session *scheduler.Session,
+	commitFunc func(),
+	syncFunc func(*scheduler.Session, []string),
+) *SystemInfoAdapter {
+	return &SystemInfoAdapter{
+		session:    session,
+		commitFunc: commitFunc,
+		syncFunc:   syncFunc,
+	}
+}
+
+// CommitSystemInfo implements SystemInfoManager
+func (a *SystemInfoAdapter) CommitSystemInfo() {
+	if a.commitFunc != nil {
+		a.commitFunc()
+	}
+}
+
+// SyncSystemInfo implements SystemInfoManager
+func (a *SystemInfoAdapter) SyncSystemInfo(keys []string) {
+	if a.syncFunc != nil {
+		a.syncFunc(a.session, keys)
+	}
+}
