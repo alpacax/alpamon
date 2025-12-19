e1f67aa92bf79f566fe1d1fe00d250c210bc7b99
Jisung Chae <js.chae@alpacax.com>
2025-12-19 14:48:24 +0900

    Fix buffer pool to use pointer indirection for proper reuse

--- pkg/tunnel/pool.go
+++ pkg/tunnel/pool.go
@@ -9,14 +9,15 @@ const copyBufferSize = 32 * 1024 // 32KB
 
 var copyBufferPool = sync.Pool{
 	New: func() interface{} {
-		return make([]byte, copyBufferSize)
+		buf := make([]byte, copyBufferSize)
+		return &buf
 	},
 }
 
 // CopyBuffered performs io.CopyBuffer using pooled buffers.
 // This reduces memory allocations and GC pressure.
 func CopyBuffered(dst io.Writer, src io.Reader) (written int64, err error) {
-	buf := copyBufferPool.Get().([]byte)
-	defer copyBufferPool.Put(buf)
-	return io.CopyBuffer(dst, src, buf)
+	bufPtr := copyBufferPool.Get().(*[]byte)
+	defer copyBufferPool.Put(bufPtr)
+	return io.CopyBuffer(dst, src, *bufPtr)
 }
